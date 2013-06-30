643a02191fff01b97414cadf9c31af86e60d3b67
cmskit <info@cms-kit.org>
2013-06-30 12:54:51 +0200

    fixed merging of avalable templates

--- backend/backend.php
+++ backend/backend.php
@@ -22,7 +22,7 @@
 *
 *  This copyright notice MUST APPEAR in all copies of the script!
 *********************************************************************************/
-/*
+/**
 * Backend-Functions
 */
 session_start();
@@ -35,6 +35,7 @@ session_regenerate_id();
 foreach($_GET as $k=>$v){  $_GET[$k]  = preg_replace('/\W/', '', $v); }
 foreach($_POST as $k=>$v){ $_POST[$k] = preg_replace('/\W/', '', $v); }
 $projectName = preg_replace('/\W/', '', $_REQUEST['project']);
+
 // reset SESSION if a Login is detected
 if (isset($_POST['project']))
 {
@@ -86,30 +87,32 @@ if (!isset($_SESSION[$projectName]) )
 	
 	
 	
-	// collect global Backend-Templates
-	$templates = glob('templates/*', GLOB_ONLYDIR);
+	// collect global Backend-Templates from  the Directory
+	$templateFolders = glob('templates/*', GLOB_ONLYDIR);
 	$config['templates'] = array();
-	foreach($templates as $template)
+	foreach ($templateFolders as $templatePath)
 	{
-		if(file_exists($template.'/backend.php'))
+		if (file_exists($templatePath.'/backend.php'))
 		{
-			$config['templates'][basename($template)] = $template;
+			$config['templates'][basename($templatePath)] = $templatePath;
 		}
 	}
 	
 	// collect some configuration-Settings from "cms"-Extensions
-	$configs = array (	
+	$configFiles = array (	
 						'extensions/cms/config/config.php',
 						$ppath . '/extensions/cms/config/config.php'
 					 );
-	foreach ($configs as $cf)
+	// load config-string
+	foreach ($configFiles as $configFile)
 	{
-		if ($s = file_get_contents($cf))
+		if ($s = file_get_contents($configFile))
 		{
 			$arr = explode('EOD', $s);
-			if(count($arr)==3 && $j = json_decode($arr[1], true)) 
+			if (count($arr)==3 && $j = json_decode($arr[1], true))
 			{
 				$config = array_merge_recursive($config, $j);
+				// print_r($config);exit(); // test if config is merged correctly (swap $configFiles above)
 			}
 		}
 	}
@@ -168,11 +171,11 @@ if (!isset($_SESSION[$projectName]) )
 	if (isset($_SESSION[$projectName]['root']))
 	{
 		$_SESSION[$projectName]['adminfolders'] = array();
-		foreach(glob('admin/*', GLOB_ONLYDIR) as $f)
+		foreach (glob('admin/*', GLOB_ONLYDIR) as $f)
 		{
 			$f = basename($f);
 			// Admin-Wizards beginning with "_" are for Super-Admins only
-			if($_SESSION[$projectName]['root']>1 || substr($f,0,1) != '_')
+			if ($_SESSION[$projectName]['root']>1 || substr($f,0,1) != '_')
 			{
 				$_SESSION[$projectName]['special']['user']['wizards'][] = 
 				array	(
@@ -220,7 +223,7 @@ $objects = $_SESSION[$projectName]['objects'];
 $lang = $_SESSION[$projectName]['lang'];
 
 // load language-array (used by Function "L")
-@include('inc/locale/'.$lang.'.php');
+@include 'inc/locale/'.$lang.'.php';
 
 // define some language-labels for JS (v.a. für Wizards)
 $tmp = array();
@@ -245,9 +248,12 @@ foreach ($objects as $objectKey => $objectValues)
 	// collect Objects in Tag-Groups
 	if (isset($objectValues['tags'][$lang]))
 	{
-		foreach($objectValues['tags'][$lang] as $t)
+		foreach ($objectValues['tags'][$lang] as $t)
 		{
-			if(!isset($objectOptions[$t[0]])) $objectOptions[$t[0]]=array();
+			if ( !isset($objectOptions[$t[0]]) )
+			{
+				$objectOptions[$t[0]] = array();
+			}
 			$objectOptions[$t[0]][] = $option;
 		}
 		
@@ -284,7 +290,7 @@ foreach ($objects as $objectKey => $objectValues)
 	
 }// collect Objects END
 
-
+//????
 ksort($objectOptions, SORT_LOCALE_STRING);
 
 $user_wizards = array_merge($_SESSION[$projectName]['config']['wizards'], $_SESSION[$projectName]['special']['user']['wizards']);
@@ -292,11 +298,10 @@ $user_wizards = array_merge($_SESSION[$projectName]['config']['wizards'], $_SESS
 $object = (!empty($_GET['object']) ? $_GET['object']: false);
 
 // define actual Template
-$_SESSION[$projectName]['template'] = (isset($_GET['template']) ? $_GET['template'] : end($_SESSION[$projectName]['config']['template']));
+$_SESSION[$projectName]['template'] = (!empty($_GET['template']) ? $_GET['template'] : end($_SESSION[$projectName]['config']['template']));
 
 //echo $_SESSION[$projectName]['template'];
 //print_r($_SESSION[$projectName]['templates']);
-
 
 // prevent caching of HTML (in addition to Meta-Tags)
 header ('Cache-Control: no-cache,must-revalidate', true);
